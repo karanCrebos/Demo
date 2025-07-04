800a5cba24b68411a7bb6535155e5b06e70d040d
karanCrebos <karanveer.crebos@gmail.com>
2025-07-04 12:55:42 +0530

    new test for browser

--- app/src/main/java/com/example/testingdemo/MainActivity.kt
+++ app/src/main/java/com/example/testingdemo/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.testingdemo.ui.theme.TestingDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestingDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    GreetingScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +45,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingScreen(modifier: Modifier) {
+    var count by remember { mutableIntStateOf(0) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Count: $count",
+                fontSize = 24.sp
+            )
+            Spacer(modifier = Modifier.height(46.dp))
+            Button(onClick = { count++ }) {
+                Text(text = "Add")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TestingDemoTheme {
-        Greeting("Android")
+//        GreetingScreen(null)
     }
 }
